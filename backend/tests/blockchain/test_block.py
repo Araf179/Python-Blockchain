b54ce4f136e932e18b9b77ac927c36e56e9f36c6
araf179 <arafshihab@outlook.com>
2021-12-02 20:19:20 -0500

    Validating blocks

---
+++ backend/tests/blockchain/test_block.py
@@ -0,0 +1,29 @@
+import time
+from backend.blockchain.block import GENESIS_DATA, Block
+from backend.config import MINE_RATE, SECONDS
+def test_mine_block():
+    last_block = Block.genesis()
+    data = 'test-data'
+    block = Block.mine_block(last_block, data)
+
+    assert isinstance(block, Block)
+    assert block.data == data
+    assert block.last_hash == last_block.hash
+
+def test_genesis():
+    genesis = Block.genesis()
+    assert isinstance(genesis, Block)
+    for key, value in GENESIS_DATA.items():
+        getattr(genesis, key) == value
+
+def test_quickly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo')
+    mined_block = Block.mine_block(last_block,  'bar')
+
+    assert mined_block.difficulty == last_block.difficulty + 1
+
+def test_slowly_mined_block():
+    last_block = Block.mine_block(Block.genesis(), 'foo')
+    time.sleep(MINE_RATE/ SECONDS)
+    mined_block = Block.mine_block(last_block, 'bar')
+    assert mined_block.difficulty == last_block.difficulty - 1
